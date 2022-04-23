5c5c38c2935410e342d33ba9a40f98709a730c43
IvanShmytov <ivanshmytov@mail.ru>
2022-04-23 10:58:49 +0300

    Добавьте файлы проекта.

---
+++ Modul8/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        string DirName = @"qwerty";
+        DirCleaner(DirName);
+    }
+    static void DirCleaner(string dirName) 
+    {
+        try
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(dirName);
+            if (dirInfo.Exists)
+            {
+                DirectoryInfo[] folderNames = dirInfo.GetDirectories();
+                foreach (var item in folderNames) 
+                {
+                    if (item.LastAccessTime.CompareTo(DateTime.Now - TimeSpan.FromMinutes(30)) < 0) 
+                    {
+                        item.Delete(true);
+                    }
+                }
+                FileInfo[] fileNames = dirInfo.GetFiles();
+                foreach (var item in fileNames)
+                {
+                    if (item.LastAccessTime.CompareTo(DateTime.Now - TimeSpan.FromMinutes(30)) < 0)
+                    {
+                        item.Delete();
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        //Console.WriteLine($"Очистка папки {dirName} от файлов и папок, не использующихся более 30 минут завершена");
+    }
+}
